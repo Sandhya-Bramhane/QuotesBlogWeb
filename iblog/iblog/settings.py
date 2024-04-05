b8b69eca739908fd3d30fbd9b26b8bd9b8e4aff9
Sandhya Bramhane <141515283+Sandhya-Bramhane@users.noreply.github.com>
2024-04-05 16:21:26 +0530

    new changes

--- iblog/iblog/settings.py
+++ iblog/iblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_g^li+7sha&&o1+mtd&67y)$dl=aqufo#5%ww@4o8@ey+anf89
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/Media/'
 
